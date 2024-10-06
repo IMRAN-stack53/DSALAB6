24cd31cca0e7f3e1422f24c760186334ced7b4c7
IMRAN-STACK53 <imranshar122@gmail.com>
2024-10-06 08:55:10 -0700

    DSAlab6

---
+++ Bublesort.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bublesort;
+
+/**
+ *
+ * @author Mujtaba Abr
+ */
+public class Bublesort {
+    int nums[]={5,1,3,4,6,2};   
+    public static void  bubbleSort(int[]nums){
+    for(int i=0; i<nums.length-1; i++ ){
+    for(int j=0; j<nums.length-1; j++){
+   if(nums[j]>nums[j+1]){
+   int temp= nums[j];
+   nums[j]=nums[j+1];
+   nums[j+1]=temp;
+   }
+   
+    }
+    }
+    }
+
+    public static void printArray(int[] nums) {
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+ public static void main (String[]args){
+       int[] nums = {5, 1, 3, 4, 6, 2};
+        bubbleSort(nums);
+        printArray(nums);
+
+
+ }
+}
